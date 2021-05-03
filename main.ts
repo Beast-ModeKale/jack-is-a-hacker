3082c8a3e56fe76b284a6296b7ff83e66491a08a
Beast-ModeKale <83430090+Beast-ModeKale@users.noreply.github.com>
2021-05-03 13:41:10 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.lightLevel())
-})
-radio.setGroup(5)
-basic.forever(function () {
-    if (input.lightLevel() < 50) {
-        radio.sendString("LIGHTS ON")
-    } else {
-        radio.sendString("LIGHTS OFF")
-    }
-    basic.pause(1000)
-})
+
